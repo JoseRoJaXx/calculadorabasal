8b184ce3359eebc6da4d74ae41600d0beb48ed7b
JoseRoJaXx <160611378+JoseRoJaXx@users.noreply.github.com>
2024-02-27 23:02:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,60 @@
-const calcular = document.getElementById("calcular");
-const error = document.getElementById("error");
-const flu = document.getElementById("flu");
-const man = document.getElementById("man");
+let button = document.getElementById("calcular");
+button.addEventListener("click", calcular);
 
-calcular.addEventListener("click", () => {
-    const data = document.getElementById("peso").value;
-    
-    if (data > 0) {
-        error.style.display = "none";
-        let flujo = calcFlujo(data);
-        let mantenimiento = flujo*1.5;
-        flu.innerHTML = flujo + " cc/hr";
-        man.innerHTML = "m+m/2 " + mantenimiento + " cc/hr";
-        flu.style.display = "block";
-        man.style.display = "block";
-    } else {
-        error.style.display = "block";
-        flu.style.display = "none";
-        man.style.display = "none";
-    }
-})
+function calcular() {
+  const imput = document.getElementById("peso");
+  const peso = parseInt(imput.value);
+  const FLU = document.getElementById("flu");
+  const MAN = document.getElementById("man");
+  const ERROR = document.getElementById("error");
 
-function calcFlujo(peso) {
-    let resto = peso;
-    let flujo = 0;
-    if (resto>20) {
-        let aux = resto-20;
-        flujo += aux*1;
-        resto -=aux;
-    }
-    if (resto>10) {
-        let aux = resto-10;
-        flujo +=aux*2;
-        resto -= aux;
-    }
-    flujo += resto*4;
-    return flujo;
+  FLU.style.display = "none";
+  MAN.style.display = "none";
+  ERROR.style.display = "none";
+
+  if (!Number.isInteger(peso) || peso <= 0) {
+    ERROR.style.display = "block";
+    imput.value = "";
+    return;
+  }
+
+
+  if (peso <= 30 ) {
+    let volumen_diario = hollidaySegar(peso);
+    let mantenimiento = Math.round(volumen_diario / 24);
+    const MM2 = Math.round(mantenimiento * 1.5);
+    console.log(volumen_diario);
+    console.log(mantenimiento, MM2);
+
+    FLU.innerHTML = mantenimiento + " cc/hr";
+    MAN.innerHTML = "m+m/2: " + MM2 + " cc/hr";
+    FLU.style.display = "block";
+    MAN.style.display = "block";
+
+  } else {
+    const SC1500 = Math.round(superficieCorporal(peso) * 1500);
+    const SC2000 = Math.round(superficieCorporal(peso) * 2000);
+    FLU.innerHTML = "SC1500: " + SC1500 + " cc/h";
+    MAN.innerHTML = "SC2000: " + SC2000 + " cc/h";
+    FLU.style.display = "block";
+    MAN.style.display = "block";
+  }
+}
+
+function hollidaySegar(peso) {
+  let volumenDiario;
+  if (peso <= 10) {
+    volumenDiario = peso * 100;
+  } else if (peso <= 20) {
+    volumenDiario = 1000 + (peso - 10) * 50;
+  } else if (peso <= 30) {
+    volumenDiario = 1500 + (peso - 20) * 20;
+  }
+  return volumenDiario;
+}
+
+function superficieCorporal(peso) {
+  let superficieCorporal = (peso * 4 + 7) / (peso + 90);
+
+  return superficieCorporal/24;
 }
